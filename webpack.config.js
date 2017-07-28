5cc41298b6fb7b604c47f01469e404cc54b1023c
Christian Rivera <chris.arevir@gmail.com>
2017-07-28 14:09:24 -0700

    added in the ability to share

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 module.exports = {
   entry: './src/ruin.js',
+
+  devServer: {
+    host: '0.0.0.0'
+  },
+
   loaders: [
     {
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel-loader',
-    },
+      loader: 'babel-loader'
+    }
   ],
+
   output: {
-    filename: 'index.js',
-  },
+    filename: 'index.js'
+  }
 };
